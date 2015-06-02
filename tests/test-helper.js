fc6e92061924142b39d7415ad1a0e7886186adf8
Cory Forsyth <cory.forsyth@gmail.com>
2015-06-02 17:59:22 -0400

    Pin addon to my branch

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -6,7 +6,7 @@ import config from "../config/environment";
 import FakeServer from 'ember-cli-fake-server';
 import MockStripe from './helpers/mock-stripe';
 import MockAnalytics from './helpers/mock-analytics';
-import storage from 'diesel/utils/storage';
+import storage from '../utils/storage';
 import MockLocation from './helpers/mock-location';
 import MockTitle from './helpers/mock-title';
 
@@ -14,6 +14,7 @@ setResolver(resolver);
 
 QUnit.testStart(function(){
   storage.remove(config.authTokenKey);
+
   MockLocation.setup();
   MockTitle.setup();
   MockStripe.setup();
